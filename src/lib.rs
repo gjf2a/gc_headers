aa2f3bd03b9357664f5cc8e532b4f2223ab132ec
gjf2a <ferrer@hendrix.edu>
2023-04-12 05:52:04 -0500

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,72 @@
+#![no_std]
+
+pub trait GarbageCollectingHeap {
+    fn new() -> Self;
+    fn load(&self, p: Pointer) -> HeapResult<u64>;
+    fn store(&mut self, p: Pointer, value: u64) -> HeapResult<()>;
+    fn malloc<T: Tracer>(&mut self, num_words: usize, tracer: &T) -> HeapResult<Pointer>;
+}
+
+pub trait Tracer {
+    fn trace(&self, blocks_used: &mut [bool]);
+}
+
+#[derive(Copy, Clone, Eq, PartialEq, Default, Debug)]
+pub struct Pointer {
+    block: usize,
+    offset: usize,
+    size: usize,
+}
+
+impl Pointer {
+    pub fn len(&self) -> usize {
+        self.size
+    }
+
+    pub fn block_num(&self) -> usize {
+        self.block
+    }
+
+    pub fn next(&self) -> Option<Self> {
+        let offset = self.offset + 1;
+        if offset < self.size {
+            Some(Self {
+                block: self.block,
+                offset,
+                size: self.size,
+            })
+        } else {
+            None
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum HeapResult<T> {
+    Ok(T),
+    Err(HeapError),
+}
+
+impl<T> HeapResult<T> {
+    pub fn unwrap(self) -> T {
+        match self {
+            HeapResult::Ok(value) => value,
+            HeapResult::Err(e) => panic!("Heap Error: {e:?}"),
+        }
+    }
+
+    pub fn map<U, F: FnOnce(T) -> U>(self, op: F) -> HeapResult<U> {
+        match self {
+            HeapResult::Ok(value) => HeapResult::Ok(op(value)),
+            HeapResult::Err(e) => HeapResult::Err(e),
+        }
+    }
+}
+
+#[derive(Clone, Copy, Eq, PartialEq, Debug)]
+pub enum HeapError {
+    OutOfBlocks,
+    OutOfMemory,
+    IllegalBlock,
+    OffsetTooBig,
+}
