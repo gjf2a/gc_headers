bbf4235d9fd84aaef321c66864c7e91a9761121e
gjf2a <ferrer@hendrix.edu>
2025-01-05 04:14:11 -0600

    Moved two more methods to the trait.

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,10 @@ pub trait GarbageCollectingHeap {
     fn store(&mut self, p: Pointer, value: u64) -> anyhow::Result<(), HeapError>;
     fn malloc<T: Tracer>(&mut self, num_words: usize, tracer: &T) -> anyhow::Result<Pointer, HeapError>;
     fn blocks_num_copies(&self) -> impl Iterator<Item = (usize, usize)>;
+    fn is_allocated(&self, block: usize) -> bool;
+    fn num_allocated_blocks(&self) -> usize {
+        self.blocks_num_copies().count()
+    }
 }
 
 pub trait Tracer {
