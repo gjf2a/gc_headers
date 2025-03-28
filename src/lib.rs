bdd43a6af1e937286901c90ce6f4273294bc847e
gjf2a <ferrer@hendrix.edu>
2025-03-28 10:03:13 -0500

    Separated GarbageCollectingHeap into three traits.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,21 @@
 #![cfg_attr(not(test), no_std)]
 use thiserror_no_std::Error;
 
-pub trait GarbageCollectingHeap {
+pub trait RamRegion {
     fn new() -> Self;
     fn load(&self, p: Pointer) -> anyhow::Result<u64, HeapError>;
     fn store(&mut self, p: Pointer, value: u64) -> anyhow::Result<(), HeapError>;
     fn address(&self, p: Pointer) -> anyhow::Result<usize, HeapError>;
-    fn malloc<T: Tracer>(&mut self, num_words: usize, tracer: &T) -> anyhow::Result<Pointer, HeapError>;
     fn blocks_num_copies(&self) -> impl Iterator<Item = (usize, usize)>;
     fn allocated_block_ptr(&self, block: usize) -> Option<Pointer>;
+}
+
+pub trait BasicRegion : RamRegion {
+    fn malloc<T: Tracer, R: RamRegion>(&mut self, num_words: usize, tracer: &T, copy_to: Option<&mut R>, higher_generation: Option<&mut R>) -> anyhow::Result<Pointer, HeapError>;
+}
+
+pub trait GarbageCollectingHeap : RamRegion {
+    fn malloc<T: Tracer>(&mut self, num_words: usize, tracer: &T) -> anyhow::Result<Pointer, HeapError>;
     fn num_allocated_blocks(&self) -> usize {
         self.blocks_num_copies().count()
     }
