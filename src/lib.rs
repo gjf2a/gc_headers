ad7797fba3ef7d7bc31a59fd76925f1de8bb6d16
gjf2a <ferrer@hendrix.edu>
2025-03-29 10:04:06 -0500

    Massive overhaul; new version of headers expects block management entirely in main heap.

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,13 @@
 #![cfg_attr(not(test), no_std)]
 use thiserror_no_std::Error;
 
-pub trait RamRegion {
+pub trait GarbageCollectingHeap {
     fn new() -> Self;
     fn load(&self, p: Pointer) -> anyhow::Result<u64, HeapError>;
     fn store(&mut self, p: Pointer, value: u64) -> anyhow::Result<(), HeapError>;
     fn address(&self, p: Pointer) -> anyhow::Result<usize, HeapError>;
     fn blocks_in_use(&self) -> impl Iterator<Item = usize>;
     fn allocated_block_ptr(&self, block: usize) -> Option<Pointer>;
-}
-
-pub trait BasicRegion: RamRegion {
-    fn malloc(
-        &mut self,
-        num_words: usize,
-        block_num: Option<usize>,
-    ) -> anyhow::Result<Pointer, HeapError>;
-}
-
-pub trait GarbageCollectingHeap: RamRegion {
     fn blocks_num_copies(&self) -> impl Iterator<Item = (usize, usize)>;
     
     fn malloc<T: Tracer>(
@@ -93,10 +82,14 @@ pub enum HeapError {
     OutOfBlocks,
     #[error("No blocks available of the requested size")]
     OutOfMemory,
-    #[error("Invalid block number: {0}")]
-    IllegalBlock(usize),
-    #[error("Invalid offset into block: {0}")]
-    OffsetTooBig(usize),
+    #[error("Block number {0} out of range; max block number is {1}")]
+    IllegalBlock(usize, usize),
+    #[error("Block not allocated: {0}")]
+    UnallocatedBlock(usize),
+    #[error("Invalid offset ({0}) into block {1}; max offset is {2}")]
+    OffsetTooBig(usize, usize, usize),
+    #[error("Invalid heap address {0}; max address is {1}")]
+    IllegalAddress(usize, usize),
 }
 
 impl core::error::Error for HeapError {}
